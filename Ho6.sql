1b32ecfcd9d5e84454a5850c551d5713f7421c30
Joshua <30993319+Joshua-18@users.noreply.github.com>
2020-11-10 13:50:57 -0600

    completed 2 of the req for prob 5
    need to test for lv_shipp := 'OK'

--- Ho6.sql
+++ Ho6.sql
@@ -72,3 +72,47 @@ SELECT day_ord_sf(dtcreated), COUNT(idBasket)
   GROUP BY day_ord_sf(dtcreated);
 /
 -- # 6-5
+CREATE OR REPLACE FUNCTION ORD_SHIP_SF 
+(p_day  NUMBER)
+RETURN VARCHAR2 
+AS 
+lv_shipp    VARCHAR2(12);
+lv_order    DATE;
+lv_sh_date  DATE;
+lv_days     NUMBER(2);
+BEGIN
+  SELECT bk.dtordered, bs.dtstage
+    INTO lv_order, lv_sh_date
+    FROM bb_basket bk, bb_basketstatus bs
+    WHERE bk.idBasket = bs.idBasket
+      AND bs.idBasket = p_day
+      AND bs.idstage = 5;
+  lv_days := lv_sh_date - lv_order;
+  IF lv_days > 1 THEN
+    lv_shipp := 'CHECK';
+  ELSE
+    lv_shipp := 'OK';
+  END IF;
+  RETURN lv_shipp;
+EXCEPTION
+  WHEN NO_DATA_FOUND THEN
+    lv_shipp := 'NOT SHIPPED';
+    RETURN lv_shipp;
+END ORD_SHIP_SF;
+/
+DECLARE
+  lv_flag VARCHAR2(12);
+  lv_num_id bb_basket.idbasket%TYPE := 3;
+BEGIN
+  lv_flag := ord_ship_sf(lv_num_id);
+  DBMS_OUTPUT.PUT_LINE('Basket ' || lv_num_id || ' is ' || lv_flag);
+END;
+/
+DECLARE
+  lv_flag VARCHAR2(12);
+  lv_num_id bb_basket.idbasket%TYPE := 13;
+BEGIN
+  lv_flag := ord_ship_sf(lv_num_id);
+  DBMS_OUTPUT.PUT_LINE('Basket ' || lv_num_id || ' is ' || lv_flag);
+END;
+/
